2b09cc0eeb0be61c532ee4d2f4841897d0010b40
Kashif <nizamanikashif2003@gmail.com>
2025-06-24 12:36:30 +0500

    added a 2nd projects page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import "./style.css";
 import { pageLoad } from "./home";
+import { projects } from "./projects";
 
+const content = document.querySelector("#content");
+const home = document.querySelector(".home");
+const project = document.querySelector(".projects");
+const contact = document.querySelector(".contact");
 
 pageLoad();
+
+project.addEventListener("click", ()=>{
+
+    content.innerHTML = "";
+    projects();
+    
+})
+
+
---
+++ src/projects.js
@@ -0,0 +1,24 @@
+export const projects = function(){
+    
+    const content = document.querySelector("#content");
+    const div = document.createElement("div");
+    div.classList.add("container");
+
+    for(let i = 1; i <= 3; i++){
+         const card = document.createElement("div");
+         card.classList.add("card");
+
+        const h1 = document.createElement("h1");
+        h1.textContent = "Project " + i;
+
+        const paragraph = document.createElement("p");
+        paragraph.textContent = "       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab necessitatibus quae dolor, dolorem similique sit natus, eius ipsa magni quia impedit eos ex quod adipisci itaque. Corporis necessitatibus sunt voluptates?";
+
+        card.appendChild(h1);
+        card.appendChild(paragraph);
+        div.appendChild(card);
+    }
+
+    content.appendChild(div)
+
+}
